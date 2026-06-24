ec7156824b23a0ce102f7f7d4e6a9aea11209c09
Maulidin <mmaulidinabdi@mail.com>
2026-06-24 17:52:53 +0800

    next fitur reset password dan insyaallah ganti service email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,11 +23,13 @@ class AuthController extends Controller
         try {
             $validator = Validator::make($request->all(), [
                 'email' => 'required|email|unique:users,email',
+                'namaLengkap' => 'required|string',
                 'password' => 'required|min:8|confirmed',
             ], [
                 'email.required' => 'Email wajib diisi',
                 'email.email' => 'Format email tidak valid',
                 'email.unique' => 'Email sudah terdaftar',
+                'namaLengkap.required' => 'Nama Lengkap Wajib diisi',
                 'password.required' => 'Password wajib diisi',
                 'password.min' => 'Password minimal 8 karakter',
                 'password.confirmed' => 'Konfirmasi password tidak sesuai',
@@ -43,15 +45,30 @@ class AuthController extends Controller
                 ], 422);
             }
 
+            $validated = $validator->validate();
+
             // Buat user baru
             $user = User::create([
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
+                'email' => $validated['email'],
+                'nama_lengkap' => $validated['namaLengkap'],
+                'password' => Hash::make($validated['password']),
             ]);
 
             try {
                 // Kirim OTP otomatis
-                $this->sendOtp($user);
+                // $this->sendOtp($user);
+                 $user->emailVerifications()->delete();
+            // Generate OTP 6 digit
+            $otpCode = str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+
+            // Simpan OTP ke database
+            $user->emailVerifications()->create([
+                'otp_code' => $otpCode,
+                'expired_at' => now()->addMinutes(5),
+            ]);
+
+            // Kirim email dengan OTP
+            Mail::to($user->email)->send(new VerifyOtpEmail($otpCode, $user->email));
             } catch (\Exception $mailException) {
                 // Jika email gagal, delete user yang baru dibuat
 
@@ -70,11 +87,12 @@ class AuthController extends Controller
                 'data' => [
                     'userId' => $user->id,
                     'email' => $user->email,
+                    'namaLengkap'=>$user->nama_lengkap,
                     'expiresIn' => '5 menit',
                 ],
             ], 201);
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            // dd($e->getMessage());
             return response()->json([
                 'success' => false,
                 'message' => 'Terjadi kesalahan: ' . $e->getMessage(),
@@ -144,7 +162,7 @@ class AuthController extends Controller
             }
 
             // $this->sendOtp($user);
-             $user->emailVerifications()->delete();
+            $user->emailVerifications()->delete();
             // Generate OTP 6 digit
             $otpCode = str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
 
@@ -290,7 +308,7 @@ class AuthController extends Controller
             $user->tokens()->delete();
 
             // Generate token
-            $token = $user->createToken('api_token',['*'], now()->addDays(1))->plainTextToken;
+            $token = $user->createToken('api_token', ['*'], now()->addDays(1))->plainTextToken;
 
             return response()->json([
                 'success' => true,
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['email', 'password','email_verified_at'])]
+#[Fillable(['email','nama_lengkap' ,'password','email_verified_at'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
